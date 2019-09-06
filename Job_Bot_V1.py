6ecff20f732bb575c0002b3c78eecef8e3114cb3
Maxime <51398749+Dragty@users.noreply.github.com>
2019-09-06 19:54:12 +0200

    Add files via upload

--- Job_Bot_V1.py
+++ Job_Bot_V1.py
@@ -62,7 +62,7 @@ def get_url(soup):
 """
 stock les url dans une liste
 """
-html=simple_get('https://candidat.pole-emploi.fr/offres/recherche?motsCles=ingenieur&offresPartenaires=true&range=0-9&rayon=10&tri=0')
+html=simple_get('https://candidat.pole-emploi.fr/offres/recherche?lieux=24R&motsCles=python&offresPartenaires=true&range=0-0&rayon=10&tri=0')
 soup= BeautifulSoup(html,'html.parser')
 def get_nb(soup):
     nb=soup.find("h1").text
@@ -74,141 +74,120 @@ listeUrl=[]
 listeF = []
 dico = {}
 
-nbOffre=int(get_nb(soup))
-
-if (nbOffre>150):
-    nbpage=(nbOffre//150)+1
-    url2=150
-else:
-    url2=nbOffre
 url1=0
-
-listeUrlT=[]
-for i in range(nbpage):
-    listeUrlT=[]
-    
-    url='https://candidat.pole-emploi.fr/offres/recherche?motsCles=ingenieur&offresPartenaires=true&range='+str(url1)+'-'+str(url2)+'&rayon=10&tri=0'
-    uc=nbOffre-url2
-    
-    if (uc>150):
-        url1+=150
-        if (url1==url2):
-            url1+=1
-        url2+=150
-    else:
-        url1=url2+1
-        url2=nbOffre
-        
-    html=simple_get(url)
-    soup=BeautifulSoup(html, 'html.parser')
-    allUrl=get_url(soup)
-    listeUrlT.append(allUrl)
-    for i in listeUrlT:
-        listeUrl.append(i) 
+nbOffre=int(get_nb(soup))
 c=0
-               
-        
-for x in listeUrl:
-    for i in x:
+nbOffre=30
+
+while(url1<=nbOffre):
+    c+=1
+    print("Liste d'url numero: ",c)
+    url2 = url1+149 if (url1+150<nbOffre) else nbOffre 
+    urlR="%d-%d" % (url1, url2)
+    
+    url='https://candidat.pole-emploi.fr/offres/recherche?lieux=24R&motsCles=python&offresPartenaires=true&range='+urlR+'&rayon=10&tri=0'
+    url=simple_get(url)
+    
+    soup=BeautifulSoup(url, 'html.parser')
+    allUrl=get_url(soup)
+    url1+=150
+    
+    o=0
+    for x in allUrl:
+        o+=1
+        print('Url numero: ',o)
         """
         boucle qui envoie les urls une par une
         """
-        skills=''
-        xp=''
         liste=[]
+        
+        raw_html = simple_get(x)
+        soup = BeautifulSoup(raw_html, 'html.parser')
     
-        raw_html = simple_get(i)
-        """
-        on récupère les éléments qui nous interesse et on les nettoie
-        """
-        title = str(BeautifulSoup(raw_html, 'html.parser').find_all("h2")[1:2])
-        title = title[31:len(title)].replace("\n</h2>]","")
+        title = soup.find("h1").text
+
+        date = soup.find("span", itemprop={"datePosted"}).text
         
-        date = str(BeautifulSoup(raw_html, 'html.parser').find_all("span", itemprop={"datePosted"}))
-        date = date[51:len(date)].replace("\n</span>]","") 
-        
-        secteur = str(BeautifulSoup(raw_html, 'html.parser').find_all("span", itemprop={"industry"}))
-        secteur = secteur[27:len(secteur)].replace('</span>]','')
-        
-        address = str(BeautifulSoup(raw_html, 'html.parser').find_all("span", itemprop={"name"}))
-        address = address[23:len(address)].split('<')[0]
-        
-        description = str(BeautifulSoup(raw_html, 'html.parser').find_all("div", itemprop={"description"}))
-        description = description[70:len(description)].replace('</p></div>]','')
-        
-        competenceXp = str(BeautifulSoup(raw_html, 'html.parser').find_all('span', attrs={"class":"skill-name"}))    
-        
-        for x in competenceXp.split('span'):
-            """
-            on récupère les elements definit dans les elses dans des variables avant de les ajouter dans une liste
-            """
-            if ("skills" in x):
-    
-                skills += x+"|"
-                skills = skills.replace('class="skill-name" itemprop="skills">','').replace('</','')          
+        try:           
+            secteur = soup.find("span", itemprop={"industry"}).text
+        except:
+            secteur='Pas de secteur'
             
-            elif ("experienceRequirements" in x):
-                xp += x.replace('</','')
-                xp = xp[54:len(xp)]
+        address = soup.find("span", itemprop={"name"}).text           
+        description = soup.find("div", itemprop={"description"}).text
         
-        contrat = str(BeautifulSoup(raw_html, 'html.parser').find_all("dd"))
-        contrat = contrat[6:len(contrat)].split('\n')[0]
-                
-        statut = str(BeautifulSoup(raw_html, 'html.parser').find_all("span", itemprop={"qualifications"}))
-        statut = statut[33:len(statut)].replace("</span>]","")
+        skills = str(soup.find_all("span", itemprop={"skills"})) 
+        skills = skills.replace('class="skill-name" itemprop="skills">','').replace('<span','').replace("<span","").replace("</span>","")    
+          
+        xp = soup.find("span", itemprop={"experienceRequirements"}).text
         
-        entreprise = str(BeautifulSoup(raw_html, 'html.parser').find_all('h4', attrs={"class":"t4 title"})[0:1]) 
+        contrat = soup.find("dd").text
+
+        try:        
+            statut = soup.find("span", itemprop={"qualifications"}).text
+        except:
+            statut = "Pas de statut"
+        
+        entreprise = str(soup.find_all('h4', attrs={"class":"t4 title"})[0:1])
         entreprise = entreprise[22:len(entreprise)].replace("</h4>]","").replace("\n","")
         
-        partenaire = str(BeautifulSoup(raw_html, 'html.parser').find("a", {"id": "idLienPartenaire"}))
+        partenaire = str(soup.find("a", {"id": "idLienPartenaire"}))
+        
         try:
             idx=partenaire.index("href=")
             idP=partenaire.index("id=")
             partenaire = partenaire[idx:idP].replace('href="','').replace('"','')
+    
         except:
             partenaire="Pas de lien"
             
-        ref = str(BeautifulSoup(raw_html, 'html.parser').find_all("span", itemprop={"value"})[0:1])
+        ref = str(soup.find_all("span", itemprop={"value"})[0:1])
         ref=ref[24:len(ref)].replace("</span>]","")
         
         """
         ajout des elems dans une liste 
         """
-        liste.append(title) 
-        liste.append(date)   
-        liste.append(secteur)           
-        liste.append(address)
-        liste.append(description)
-        liste.append(skills)
-        liste.append(xp)
-        liste.append(contrat)
-        liste.append(statut)
-        liste.append(entreprise)
-        liste.append(partenaire)
-        liste.append(ref)
         
+        listeCol=[title,date,secteur,address,description,skills,xp,contrat,statut,entreprise,partenaire,ref]
+        motclef=""
+        
+        for a in listeCol:
+            
+            liste.append(a)
+
+            if ((a.find("sql"))>=0 or (a.find("python"))>=0  or (a.find("java"))>=0 or (a.find(" r "))>=0):
+                
+                if ((a.find("sql"))>=0 ):
+                    motclef+="sql "
+                if ((a.find("python"))>=0 ):
+                    motclef+="python "
+                if ((a.find("java"))>=0):
+                    motclef+="java "
+                if ((a.find(" r "))>=0):
+                    motclef+="r "  
+            
+        liste.append(motclef)
+       
         """
         ajout de chaque liste dans une autre liste ( double liste pour faire )
         """
+        
         listeF.append(liste)
-        c+=1
-        print(c)
-c=0
+
 for i in listeF:
+    
     """
     transformation de la liste en dictionnaire
     """
-    date = str(datetime.datetime.now())  
-    c += 1
-    dico[date] = i
     
+    date = str(datetime.datetime.now())  
+    dico[date] = i
+
 """
 transformation du dictionnaire en df avec les clés en index
 """
 
-
-df= pd.DataFrame.from_dict(dico,orient=u'index',columns=[u"Intitule",u"Date",u"Secteur_Activite",u"Adresse",u"Description",u"Competences",u"Experience",u"Type_Contrat",u"Statut",u"Entreprise",u"Lien_Partenaire",u"Reference"])
-
+df= pd.DataFrame.from_dict(dico,orient=u'index',columns=[u"Intitule",u"Date",u"Secteur_Activite",u"Adresse",u"Description",u"Competences",u"Experience",u"Type_Contrat",u"Statut",u"Entreprise",u"Lien_Partenaire",u"Reference",u"Mot_Clef"])
 print(df)
 
 #
